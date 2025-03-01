4091cebfea6e0976f2d92f33783d406fff6ca55e
John Nasseh <jnasseh7801@gmail.com>
2025-03-01 12:23:22 -0500

    removed least and greatest

--- backend/sql/schema/02_friends.sql
+++ backend/sql/schema/02_friends.sql
@@ -6,5 +6,5 @@ CREATE TABLE Friends (
     Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_friends_user1 FOREIGN KEY (User1ID) REFERENCES User(UserID) ON DELETE CASCADE, --foreign key that makes sure that user1 exists. if user is deleted, all friendships involving them are also deleted
     CONSTRAINT fk_friends_user2 FOREIGN KEY (User2ID) REFERENCES User(UserID) ON DELETE CASCADE,
-    constraint unique_friendship_pair UNIQUE (LEAST(User1ID, User2ID), GREATEST(User1ID, User2ID)) -- makes sure there are no duplicate friendships. LEAST and GREATEST are used to store them in a consistent order   
+    CONSTRAINT unique_friendship_pair UNIQUE (User1ID, User2ID) -- makes sure there are no duplicate friendships
 );
