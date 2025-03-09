eadd1b1233ac89137eeeb873e77c0662cae669fb
asr27 <asr27@njit.edu>
2025-03-09 05:10:44 -0400

    Added rough ui of event form

---
+++ frontend/src/app/events/create/page.tsx
@@ -0,0 +1,9 @@
+import { EventForm } from "@/components/event-form";
+const EventCreationPage = () => {
+    return (
+        <div>
+            <EventForm/>
+        </div>
+    )
+}
+export default EventCreationPage;
