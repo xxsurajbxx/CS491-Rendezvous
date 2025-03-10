bcf4f1f3dd79483f4649b0b4725ebb56c9ad675c
Jordan Bruce <jordanbruce@Jordans-MacBook-Air.local>
2025-03-10 11:22:38 -0400

    Added the initial folders for the friends page.

---
+++ frontend/src/features/friends/components/friends-screen.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export const FriendsScreen = () => {
+
+  return(
+    <div className="h-full flex items-center justify-center bg-[#5C3B58]">
+      <h1>Hello from friends page!!!</h1>
+    </div>
+  );
+}
