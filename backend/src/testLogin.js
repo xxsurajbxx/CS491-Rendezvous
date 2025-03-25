54fba09cf8a9a49f3665406a76f7bd4392741a4b
John Nasseh <jnasseh7801@gmail.com>
2025-03-24 23:10:57 -0400

    login testing file

--- backend/src/testLogin.js
+++ backend/src/testLogin.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:5000/api/login", {
+fetch("http://localhost:8080/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email: "testuser@example.com", password: "testpassword" })
