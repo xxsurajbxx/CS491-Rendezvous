7f0eb7e352775f77ba505e5aa14a6b70e7e684ab
John Nasseh <jnasseh7801@gmail.com>
2025-03-29 21:31:37 -0400

    routes for friends

---
+++ backend/src/friendRoutes.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { addFriend } from "./addFriendsEndpoint";
+import { viewFriendRequests } from "./viewFriendRequests";
+import { respondToRequest } from "./friendRequestActions";
+import { getFriends } from "./friendsListEndpoint";
+import { deleteFriend } from "./deleteFriendEndpoint";
+const router = express.Router();
+
+// add friend
+router.post("/add", async (req, res) => {
+    await addFriend(req, res);
+});
+
+// view pending friend requests
+router.get("/requests/:userId", async (req, res) => {
+    await viewFriendRequests(req, res);
+});
+
+// respond to a request (accept or reject)
+router.post("/respond/:friendId", async (req, res) => {
+    await respondToRequest(req, res);
+});
+
+// view all accepted friends
+router.get("/all/:userId", async (req, res) => {
+    await getFriends(req, res);
+});
+
+// delete a friend
+router.delete("/delete/:friendId", async (req, res) => {
+    await deleteFriend(req, res);
+});
+
+export default router;
