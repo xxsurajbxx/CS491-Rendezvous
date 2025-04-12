b71c94865790dc0c2990ef50ab2bf14dd043bc96
John Nasseh <jnasseh7801@gmail.com>
2025-04-12 17:15:52 -0400

    username testing

--- backend/src/test_files/testUpdateUserProfile.js
+++ backend/src/test_files/testUpdateUserProfile.js
@@ -2,7 +2,7 @@ fetch("http://localhost:8080/api/update-profile/18", {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      name: "johnupdatedtest2",
+      username: "johnupdatedtest2",
       address: "", // leave fields empty if you dont want to change them
       description: "test", // or you can leave it to what its currently set to and it wont change it because it detects
       currentPassword: "",
