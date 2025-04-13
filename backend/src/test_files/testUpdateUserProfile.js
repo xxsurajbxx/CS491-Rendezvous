a682aaba8476f593dd0de15dc7f73649bb5e3a02
John Nasseh <jnasseh7801@gmail.com>
2025-04-13 17:00:54 -0400

    updating test file

--- backend/src/test_files/testUpdateUserProfile.js
+++ backend/src/test_files/testUpdateUserProfile.js
@@ -2,9 +2,9 @@ fetch("http://localhost:8080/api/update-profile/18", {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      username: "johnupdatedtest2",
-      address: "", // leave fields empty if you dont want to change them
-      description: "test", // or you can leave it to what its currently set to and it wont change it because it detects
+      username: "",
+      address: "newark, new jersey", // leave fields empty if you dont want to change them
+      description: "", // or you can leave it to what its currently set to and it wont change it because it detects
       currentPassword: "",
       newPassword: ""
     })
