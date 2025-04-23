bc628835bc86eb5b3c61e48d9aef3ce04180648c
asr27 <asr27@njit.edu>
2025-04-22 20:55:57 -0400

    Conditionally show notification based on if profile is user's

--- frontend/src/app/users/[userId]/page.tsx
+++ frontend/src/app/users/[userId]/page.tsx
@@ -1,20 +1,28 @@
-import NavigationBar from "@/components/navigation-bar";
-import { getTokenPayload } from "../../../../utils/auth";
-import UserScreen from "@/components/user-screen";
-import { redirect } from "next/navigation";
+import NavigationBar from "@/components/navigation-bar"
+import { getTokenPayload } from "../../../../utils/auth"
+import UserScreen from "@/components/user-screen"
+import { redirect } from "next/navigation"
 
-
-
-export default async function UserPage() {
-  const token = await getTokenPayload();
+export default async function UserPage({
+  params,
+}: {
+  params: Promise<{ userId: string }>
+}) {
+  const token = await getTokenPayload()
   if (!token) {
-    redirect("/auth");
-  } else { 
-        return (
-        <div>
-            <NavigationBar/>
-            <UserScreen id={token.userId}/>
-        </div>
-        );
+    redirect("/auth")
   }
+
+  // Await the params Promise to get the userId
+  const { userId } = await params
+
+  // Check if the user is viewing their own profile
+  const isOwnProfile = userId === token.userId.toString()
+
+  return (
+    <div>
+      <NavigationBar hideNotification={isOwnProfile} />
+      <UserScreen id={token.userId} />
+    </div>
+  )
 }
