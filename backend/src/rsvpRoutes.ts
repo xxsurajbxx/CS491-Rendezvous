da998b33cc2f978174d859427d5f882e906e10e9
John Nasseh <jnasseh7801@gmail.com>
2025-04-26 19:50:11 -0400

    status support and changed cancel rsvp route

--- backend/src/rsvpRoutes.ts
+++ backend/src/rsvpRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { rsvpToEvent } from "./rsvpToEventEndpoint";
 import { cancelRSVP } from "./rsvpCancelEndpoint";
 import { listRSVPs } from "./rsvpListEndpoint";
+import { getRsvpStatus } from "./getRSVPStatus";
 
 const router = express.Router();
 
@@ -11,13 +12,18 @@ router.post("/", async (req, res) => {
   });
   
   // cancel rsvp
-  router.delete("/", async (req, res) => {
+  router.delete("/:userId/:eventId", async (req, res) => {
     await cancelRSVP(req, res);
   });
   
   // list rsvps
   router.get("/", async (req, res) => {
     await listRSVPs(req, res);
+  });
+
+  // get rsvp status
+  router.get("/status/:userId/:eventId", async (req, res) => {
+    await getRsvpStatus(req, res);
   });
 
 export default router;
