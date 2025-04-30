cd2209439edb3642e3c5a758aaf6af9d315f21fd
Suraj Bhardwaj <xxsurajbxx@mac.resource.campus.njit.edu>
2025-04-30 17:47:36 -0400

    fixed the authendpoints.ts file

--- backend/src/authEndpoint.ts
+++ backend/src/authEndpoint.ts
@@ -41,7 +41,7 @@ export const authenticateUser = async (email: string, password: string, existing
           }
 
         const token = jwt.sign(
-            { userId: user.UserID, email: user.Email, name: user.Name, address: user.Address },
+            { userId: user.UserID, email: user.Email, name: user.Name, address: user.Address, verified: user.IsVerified },
             JWT_SECRET,
             { expiresIn }
         );
